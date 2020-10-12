eddd1f09ef4d0d61bcbfb46ea33b3c252c9efc2b
A Rod <ace@AceMBPro.local>
2020-10-11 21:49:56 -0400

    graph basic ui functional

--- src/app/friend/graph/graph.component.ts
+++ src/app/friend/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
 import { Contact } from 'src/app/models/contact/contact.model';
 import { Friend } from 'src/app/models/friend/friend.model';
@@ -15,7 +15,7 @@ import { mergeMap } from 'rxjs/operators';
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.scss']
 })
-export class GraphComponent implements OnChanges, OnInit {
+export class GraphComponent implements OnChanges, OnInit, AfterViewInit {
 
   // event for toggling a selection of a node
   @Output() select: EventEmitter<string> = new EventEmitter<string>();
@@ -29,10 +29,51 @@ export class GraphComponent implements OnChanges, OnInit {
   ) {
    }
 
+  ngAfterViewInit(): void {
+    // enable dragging
+    d3.select(this.graphContainer.nativeElement)
+      .call(d3.drag()
+        .container(this.graphContainer.nativeElement).subject(this.dragsubject)
+        .on("start", this.dragstarted)
+        .on("drag", this.dragged)
+        .on("end", this.dragended));
+
+    d3.select(this.graphContainer.nativeElement)
+    .on('click', this.selectNode)  
+  }
+
   private simulation = d3.forceSimulation();
 
   ngOnInit(): void {
 
+  }
+
+  private selectNode = () => {
+    const point = d3.mouse(this.graphContainer.nativeElement);
+    const node = this.simulation.find(point[0], point[1], 20) as Friend;
+
+    if (node) this.select.emit(node.id);
+  }
+
+  private dragsubject = () => {
+    return this.simulation.find(d3.event.x, d3.event.y);
+  }
+
+  private dragstarted = () => {
+    if (!d3.event.active) this.simulation.alphaTarget(1).restart();
+    d3.event.subject.fx = d3.event.subject.x;
+    d3.event.subject.fy = d3.event.subject.y;
+  }
+
+  private dragged = () => {
+    d3.event.subject.fx = d3.event.x;
+    d3.event.subject.fy = d3.event.y;
+  }
+
+  private dragended = () => {
+    if (!d3.event.active) this.simulation.alphaTarget(0);
+    d3.event.subject.fx = null;
+    d3.event.subject.fy = null;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -47,8 +88,13 @@ export class GraphComponent implements OnChanges, OnInit {
 
     const updatedNodes = setMinusSet(nodes, nodesToRemove).concat(nodesToAdd);
 
-    const edges = this.edges.map(edge => ({ source: edge.fromId, target: edge.toId }))
+    const edges = this.edges.map(edge => {
+      const from = updatedNodes.findIndex(node => node.id === edge.fromId)
+      const to = updatedNodes.findIndex(node => node.id === edge.toId)
 
+      return {source:updatedNodes[from], target:updatedNodes[to]};
+    })
+    
     this.simulation.nodes(updatedNodes)
       .force('charge', d3.forceManyBody())
       .force('link', d3.forceLink(edges))
@@ -58,10 +104,15 @@ export class GraphComponent implements OnChanges, OnInit {
   }
 
   get graph() {
-    const nodes = this.simulation.nodes();
+    const nodes = this.simulation.nodes().map(n => (<Friend>{...n}));
     const selectedNodes = [];
 
-    const edges = this.edges.map(edge => ([edge.fromId, edge.toId]))
+    const edges = this.edges.map(edge => {
+      const from = nodes.findIndex(node => node.id === edge.fromId)
+      const to = nodes.findIndex(node => node.id === edge.toId)
+
+      return [nodes[from], nodes[to]];
+    })
 
     return { nodes, edges, selectedNodes }
   }
